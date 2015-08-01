fac61b72e02738f00413ce850b1117a30cb4563f
Pierce Moore <me@prex.io>
2015-07-31 20:29:34 -0500

    Load Eyes from DB to build dynamic dashboard page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class DashboardController < ApplicationController
   def index
+    @eyes = Eye.where(created_by: current_user.id)
+
+    puts "Eyes located for current_user"
+    puts @eyes
   end
 end
